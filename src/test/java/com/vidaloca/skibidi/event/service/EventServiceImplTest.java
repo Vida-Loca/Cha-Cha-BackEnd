ccd7c279d047809212b2babfc0d5f55f3174a542
janlew <janlew97@gmail.com>
2020-05-03 13:50:06 +0200

    fixed merge

--- src/test/java/com/vidaloca/skibidi/event/service/EventServiceImplTest.java
+++ src/test/java/com/vidaloca/skibidi/event/service/EventServiceImplTest.java
@@ -154,14 +154,8 @@ class EventServiceImplTest {
 
         when(userRepository.findById(anyLong())).thenReturn(Optional.of(user));
         when(eventRepository.save(any(Event.class))).thenReturn(event);
-<<<<<<< HEAD
         when(addressRepository.findByCountryAndCityAndPostcodeAndStreetAndNumberAndLatitudeAndLongitude(anyString(),
                 anyString(), anyString(), anyString(), anyString(),anyFloat(),anyFloat())).thenReturn(addressOptional);
-=======
-        when(addressRepository.findByCountryAndCityAndPostcodeAndStreetAndNumberAAndLatitudeAndLongitude(anyString(),
-                anyString(), anyString(), anyString(), anyString(), anyFloat(), anyFloat())).thenReturn(addressOptional);
->>>>>>> 64ad5277035912a194e56a020d31836fed58d99b
-
         //when
         Event returnedEvent = eventService.addNewEvent(eventDto, 1L);
 
